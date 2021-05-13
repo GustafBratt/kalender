cb47f4ff0b9845f94539d4020b52946512d26bab
Gustaf Bratt <Gustaf.Bratt@cybercom.com>
2021-05-13 18:00:59 +0200

    småfixar

--- src/test/java/com/cybercom/gustafbratt/kalender/KalenderApplicationTests.java
+++ src/test/java/com/cybercom/gustafbratt/kalender/KalenderApplicationTests.java
@@ -8,17 +8,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("apitest")
 public class KalenderApplicationTests {
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
-	public void getRequest() {
+	public void helloOk() {
 		RestAssuredMockMvc.mockMvc(mvc);
 
 		MockMvcResponse response = RestAssuredMockMvc.given()
@@ -28,6 +30,7 @@ public class KalenderApplicationTests {
 				.then()
 				.extract().response();
 
+		System.out.println(response.asString());
 		Assertions.assertEquals(200, response.statusCode());
 		Assertions.assertTrue(response.asString().contains("2021-05-13"));
 	}
